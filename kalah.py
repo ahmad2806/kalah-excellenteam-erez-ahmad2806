a83e7d0e2d504affa5d3519b6a3f76a55ed11f8c
ahmad2806 <ahmadhashem2806@gmail.com>
2018-11-20 19:19:22 +0200

    1.1 implement illegal hole

--- kalah.py
+++ kalah.py
@@ -2,4 +2,14 @@ class Kalah(object):
     def __init__(self, holes, seeds):
         self.kalah_board = [seeds if not i == holes and not i == ((holes * 2) + 1) else 0 for i in
                             range(0, (holes * 2) + 2)]
+        self.player_1 = 0
+        self.player_2 = 0
+        self.holes_count = holes;
+        self.player_one_bank = holes
+        self.player_two_bank = holes * 2 + 1
+        self.player_turn = True
 
+    def play(self, hole):
+        if hole < 0 or hole > self.holes_count:
+            print("illegal hole number")
+            return

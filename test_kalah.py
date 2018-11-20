4bb38064d25e17aa9b69e9e2ded7257e90c19669
ahmad2806 <ahmadhashem2806@gmail.com>
2018-11-20 21:21:52 +0200

    1.2 Empty Hole

--- test_kalah.py
+++ test_kalah.py
@@ -6,7 +6,8 @@ class KalahTestCase(unittest.TestCase):
 
     def setUp(self):
         self.game = Kalah(6, 4)
-        self.game1 = Kalah(4, 6)
+        self.game1 = Kalah(5, 5)
+        self.game2 = Kalah(4, 6)
 
     def tearDown(self):
         pass
@@ -62,12 +63,21 @@ class KalahTestCase(unittest.TestCase):
         self.assertEqual(self.game.player_turn, True)
 
     def test_cross_other_bank(self):
-        self.assertEqual(self.game1.player_turn, True)
-        self.assertEqual(self.game1.kalah_board, [6, 6, 6, 6, 0, 6, 6, 6, 6, 0])
-        self.game1.play(3)
-        self.assertEqual(self.game1.kalah_board, [7, 6, 6, 0, 1, 7, 7, 7, 7, 0])
-        self.assertEqual(self.game1.player_turn, False)
+        self.assertEqual(self.game2.player_turn, True)
+        self.assertEqual(self.game2.kalah_board, [6, 6, 6, 6, 0, 6, 6, 6, 6, 0])
+        self.game2.play(3)
+        self.assertEqual(self.game2.kalah_board, [7, 6, 6, 0, 1, 7, 7, 7, 7, 0])
+        self.assertEqual(self.game2.player_turn, False)
 
+    def test_empty_hole(self):
+        self.assertEqual(self.game.player_turn, True)
+        self.assertEqual(self.game.kalah_board, [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0])
+        self.game.play(2)
+        self.assertEqual(self.game.kalah_board, [4, 4, 0, 5, 5, 5, 1, 4, 4, 4, 4, 4, 4, 0])
+        self.assertEqual(self.game.player_turn, True)
+        self.game.play(2)
+        self.assertEqual(self.game.kalah_board, [4, 4, 0, 5, 5, 5, 1, 4, 4, 4, 4, 4, 4, 0])
+        self.assertEqual(self.game.player_turn, True)
 
 if __name__ == '__main__':
     unittest.main()
